4840529f1fba9780b6ed2153f8fe7fe7f05df74d
OUMontiel <omarume@gmail.com>
2022-10-19 19:33:44 -0500

    Add database models with SQLite and Sequelize

---
+++ taho-server/src/db/models.js
@@ -0,0 +1,190 @@
+import { Model, DataTypes } from 'sequelize';
+
+export class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastNmae: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'users',
+        timestamps: true
+    }
+);
+
+export class Worker extends Model {};
+
+Worker.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastNmae: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rfc: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        certificate: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        services: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'workers',
+        timestamps: true
+    }
+);
+
+export class Job extends Model {};
+
+Job.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: User,
+              key: 'id',
+            },
+            allowNull: false,
+        },
+        workerId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: Worker,
+              key: 'id',
+            },
+            allowNull: false,
+        },
+        stars: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        review: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'jobs',
+        timestamps: true
+    }
+)
+
+export class Award extends Model {};
+
+Award.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'awards',
+        timestamps: true
+    }
+);
+
+export class WorkerAward extends Model {};
+
+WorkerAward.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        workerId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: Worker,
+              key: 'id',
+            },
+            allowNull: false,
+        },
+        awardId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: Award,
+              key: 'id',
+            },
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'workerAwards',
+        timestamps: true
+    }
+);
--- taho-server/src/index.js
+++ taho-server/src/index.js
@@ -1,13 +1,18 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import { sequelize } from './db';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-});
+(async () => {
+    await sequelize.sync({ force: true});
+
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}`);
+    });
+})
+
